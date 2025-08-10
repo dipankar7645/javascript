ce64d8c1548e2a4f42f48bb65fd6eb738e1d7014
dipankar7645 <dipankar11092003@gmail.com>
2025-08-10 21:29:35 +0530

    commit

--- Question/firstelmremove.js
+++ Question/firstelmremove.js
@@ -1,3 +1,5 @@
-let arr = [3,5,1,6,5]
-let newArr = arr.slice(1)
-console.log(newArr)
+// let arr = [3,5,1,6,5]
+// let newArr = arr.slice(1)
+// console.log(newArr)
+
+
---
+++ Question/js/index.js
@@ -0,0 +1,9 @@
+1.
+// console.log("Dipankar Kumar" , "my hobbie is coding")
+
+2.
+// console.log(45*2-10)
+
+3.
+const dt = new Date();
+console.log(dt.getFullYear());
